1900716566788a9cca937eb9d8aed0f9f4a6eaea
UtpalB-arch <67811773+UtpalB-arch@users.noreply.github.com>
2021-01-25 06:08:47 +1100

    Update telephone_valid.js

--- telephone_valid.js
+++ telephone_valid.js
@@ -1,5 +1,6 @@
 function telephoneCheck(str){
-    let rex3 = /^(1[\s]?)?((\([2-9][0-9][0-9]\))|([2-9][0-9][0-9]))([\s\-]?)?[2-9][0-9][0-9]([\s\-]?)?[2-9][0-9][0-9][0-9]$/;          
+    
+    XXXXXXXXXXXXXXXXXXXXX PORTION OF CODE DELETED TO PROTECT INTELLECTUAL PROPERTY XXXXXXXXXXXXXXXXXXXXX       
     
     return rex3.test(str);
 }
